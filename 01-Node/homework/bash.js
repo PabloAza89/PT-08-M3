cbd4fcd545db40cd7867ed32f0a422d7206bc24d
Juan Pablo Azambuyo <juanpabloazambuyo@gmail.com>
2022-09-06 18:03:01 -0300

    w

---
+++ 01-Node/homework/bash.js
@@ -0,0 +1,9 @@
+    // Output un prompt
+    process.stdout.write('prompt > ');
+    // El evento stdin 'data' se dispara cuando el user escribe una línea
+    process.stdin.on('data', function (data) {
+      var cmd = data.toString().trim(); // remueve la nueva línea
+      process.stdout.write('You typed: ' + cmd);
+      process.stdout.write('\nYou typed: ' + cmd);
+      process.stdout.write('\nprompt > ');
+    });
